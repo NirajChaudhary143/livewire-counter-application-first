42904ab869345eb9dc434cb6d63d2447a5a236af
NirajChaudhary143 <nirajchy2000@gmail.com>
2023-06-07 13:17:53 +0545

    livewire-counter-application

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,6 @@
+<div style="text-align: center;">
+    <h1>Caounter Application</h1>
+    <button wire:click="increment">+</button>
+    <p>{{ $count }}</p>
+    <button wire:click="decrement">-</button>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@livewireStyles
+
+    <title>Livewire Example</title>
+</head>
+<body>
+    
+    <livewire:counter/>
+@livewireScripts
+</body>
+</html>
